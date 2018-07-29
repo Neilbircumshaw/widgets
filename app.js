85a14c64f02317f1a58d76caef81eb9410941bc3
Neilbircumshaw <n.bircumshaw@hotmail.co.uk>
2018-07-29 23:04:37 +0100

    3 pop up boxes

---
+++ app.js
@@ -0,0 +1,23 @@
+
+// --------------first helper---------------
+$( ".question" ).click(function() {
+  $( ".questionAnswer").toggleClass( "showAndHide" )});
+
+// --------------second helper---------------
+  $( ".question2" ).click(function() {
+    $( ".questionAnswer2").toggleClass( "show" );
+
+    if ( $(!".show")) { //did this so that the hide animation doesnt start immediatly
+  $( ".questionAnswer2" ).addClass( "hide" )}
+  else { $( ".questionAnswer2" ).removeClass( "hide" )}; //not sure why it's not removing hide, still works though
+  });
+
+  // --------------third helper---------------
+  $( ".question3" ).click(function() {
+    $( ".questionAnswer3").toggleClass( "showAndHide2" )});
+
+    $( ".question3" ).click(function() {
+      $( ".smallBubble").toggleClass( "showAndHide2" )});
+
+      $( ".question3" ).click(function() {
+        $( ".mediumBubble").toggleClass( "showAndHide2" )});
